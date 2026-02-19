a030363fec59526e90793e373562843670490f66
llaa33219 <lukems903@gmail.com>
2026-02-19 19:12:12 +0900

    Upgrade wrangler to v4, remove kv-asset-handler, simplify to [assets] config

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import { getAssetFromKV } from '@cloudflare/kv-asset-handler';
-
 export default {
-  async fetch(request, env, ctx) {
-    try {
-      return await getAssetFromKV(
-        {
-          request,
-          waitUntil: ctx.waitUntil.bind(ctx),
-        },
-        {
-          ASSET_NAMESPACE: env.__STATIC_CONTENT,
-          ASSET_MANIFEST: env.__STATIC_CONTENT_MANIFEST,
-        }
-      );
-    } catch (e) {
-      return new Response('Not Found', { status: 404 });
-    }
+  async fetch(request, env) {
+    return env.ASSETS.fetch(request);
   },
 };

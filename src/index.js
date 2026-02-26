6a8cb5019c6027dca9e957d2fd061c15b281d94d
llaa33219 <lukems903@gmail.com>
2026-02-26 20:34:35 +0900

    디자인 시스템 적용 및 기업 소개 페이지 구축
    - index.html: 네오브루탈리즘 디자인 시스템 전면 적용 (하드 쉐도우, 22px 본문, 1280px 컨테이너)
    - privacy.html: 개인정보처리방침 페이지 추가 (AdSense 승인용)
    - robots.txt, sitemap.xml: SEO 최적화
    - src/index.js: /privacy 클린 URL 라우팅 추가
    - 반응형 브레이크포인트 수정 (1023px/639px)
    - Google Sans Code 폰트 임포트 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 export default {
   async fetch(request, env) {
+    const url = new URL(request.url);
+    if (url.pathname === '/privacy') {
+      return env.ASSETS.fetch(new Request(new URL('/privacy.html', url.origin), request));
+    }
     return env.ASSETS.fetch(request);
   },
 };
